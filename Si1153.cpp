a817256b07a36a9393aa90248f76ca5a1724f0cf
PureF <furqaan@pureengineering.com>
2017-03-23 10:10:47 -0700

    Update Si1153.cpp

--- Si1153.cpp
+++ Si1153.cpp
@@ -78,9 +78,12 @@ int Si1153::param_query(uint8_t loc){
 
     while(1){
         int cmmnd_ctr = Si1153::read_register(Si1153::DEVICE_ADDRESS, Si1153::RESPONSE_0, 1);
+	    
         packet[0] = Si1153::COMMAND;
         packet[1] = loc | (0B01 << 6);
+	    
         Si1153::write_data(Si1153::DEVICE_ADDRESS, packet, sizeof(packet));
+	    
         int r = Si1153::read_register(Si1153::DEVICE_ADDRESS, Si1153::RESPONSE_0, 1);
         if(r > cmmnd_ctr){
             result = Si1153::read_register(Si1153::DEVICE_ADDRESS, Si1153::RESPONSE_1, 1);
@@ -98,9 +101,12 @@ void Si1153::send_command(uint8_t code){
 
     while(1){
         int cmmnd_ctr = Si1153::read_register(Si1153::DEVICE_ADDRESS, Si1153::RESPONSE_0, 1);
+	    
         packet[0] = Si1153::COMMAND;
         packet[1] = code;
+	    
         Si1153::write_data(Si1153::DEVICE_ADDRESS, packet, sizeof(packet));
+	    
         int r = Si1153::read_register(Si1153::DEVICE_ADDRESS, Si1153::RESPONSE_0, 1);
         if(r > cmmnd_ctr)
             break;  
@@ -113,9 +119,11 @@ void Si1153::send_command(uint8_t code){
 int Si1153::get_int_from_bytes(uint8_t *data, size_t len){
     int result = 0;
     int shift = 8 * len;
+	
     for(int i = 0; i < len; i++){
         shift -= 8;
         result |= ((data[i] << shift) & (0xFF << shift));
     }
+	
     return result;
 }
